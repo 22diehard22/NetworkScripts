08917216c098659c3aa91c4382372076254a7e52
Josh <josh.majzner@gmail.com>
2018-02-26 18:21:17 -0600

    Add files via upload
    Simple Ping script to check network connectivity.

---
+++ ping.cs
@@ -0,0 +1,17 @@
+﻿
+// Return true if ping  = success
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            return pingable;
+        }
